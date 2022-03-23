fc5e98ecb6a3792261596abf38f2d80e3275a4fc
Everton Rodrigues <102188292+everton1999@users.noreply.github.com>
2022-03-23 20:45:36 -0300

    Update exercíciospython.py

--- exercíciospython.py
+++ exercíciospython.py
@@ -21,8 +21,3 @@ while True:
     if notas <= len(dados) - 1:
         print(f'Notas de {dados[notas][0]} são {dados[notas][2]}')
 print('Volte Sempre!')
-
-#Exercício Python 089:
-# Crie um programa que leia nome e duas notas de vários alunos e guarde tudo em uma lista composta.
-#No final, mostre um boletim contendo a média de cada um e permita que o usuário possa mostrar as
-#notas de cada aluno individualmente.
